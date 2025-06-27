ffef73316bea1f00b86a729b5258d9b67077b2d2
Ricky <ricardojsribeiro99@gmail.com>
2025-06-27 18:28:58 +0100

    About section

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import About from "@/components/About";
 import Cocktails from "@/components/Cocktails";
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
@@ -12,7 +13,7 @@ export default function Home() {
             <Navbar />
             <Hero />
             <Cocktails />
-            <div className="h-screen"></div>
+            <About />
         </main>
     );
 }
